64bdc87d129d52d365b78d972f22c3698f6759f0
Mamuche <marionlaroche17@gmail.com>
2025-07-04 15:05:31 +0200

    task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,10 +11,36 @@ interface Directors extends Teacher {
 	numberOfReports: number;
 }
 
-function printTeacher(firstName: string, lastName: string) {
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
 	return (firstName.charAt(0) + ". " + lastName)
 };
 
 interface printTeacherFunction {
 	(firstName: string, lastName: string) : string;
+}
+
+interface StudentClassClass {
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+interface StudentClassconstructor {
+	new (firstName: string, lastName: string): StudentClassClass;
+}
+class StudentClass {
+	firstName: string;
+	lastName: string;
+
+		constructor(firstName: string, lastName: string) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+
+		workOnHomework(): string {
+			return "Currently working";
+		}
+
+		displayName():string {
+			return this.firstName;
+		}
 }
