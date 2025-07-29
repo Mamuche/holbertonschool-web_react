33f4b8d6ca256b8a3d09181e9f5b1408a4fed258
Mamuche <marionlaroche17@gmail.com>
2025-07-29 12:04:52 +0200

    rebase

--- react_intro/task_3/dashboard/src/utils.spec.js
+++ react_intro/task_3/dashboard/src/utils.spec.js
@@ -2,20 +2,20 @@ import { getCurrentYear, getFooterCopy, getLatestNotification } from './utils';
 
 
 describe('utils functions', () => {
-  it('getCurrentYear returns the current year', () => {
+  test('getCurrentYear returns the current year', () => {
     const currentYear = new Date().getFullYear();
     expect(getCurrentYear()).toBe(currentYear);
   });
 
-  it('getFooterCopy returns "Holberton School" if it s true', () => {
+  test('getFooterCopy returns "Holberton School" if it s true', () => {
     expect(getFooterCopy(true)).toBe('Holberton School');
   });
 
-  it('getFooterCopy returns "Holberton School main dashboard" if it s false', () => {
+  test('getFooterCopy returns "Holberton School main dashboard" if it s false', () => {
     expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
   });
 
-  it('getLatestNotification returns string', () => {
+  test('getLatestNotification returns string', () => {
     expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
   });
 });
