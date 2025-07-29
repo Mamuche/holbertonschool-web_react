ece2d5cc19ff31b405579266d20dfe4369be524e
Mamuche <marionlaroche17@gmail.com>
2025-07-29 10:45:17 +0200

    task4

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -22,10 +22,10 @@ function App() {
         <div className="App-body">
           <p>Login to access the full dashboard</p>
           <form>
-            <label htmlFor="email">Email:</label>
+            <label htmlFor="email">Email</label>
             <input type="email" id="email" name="email" />
 
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" name="password" />
 
             <button type="submit">OK</button>
--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -23,4 +23,23 @@ describe('App component', () => {
     const logoImg = screen.getByAltText(/holberton logo/i);
     expect(logoImg).toBeInTheDocument();
   });
+
+  test('contains 2 inputs', () => {
+    const emailInput = screen.getByLabelText(/email/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  test('contains 2 labels with text "Email" and "Holberton"', () => {
+    const emailLabel = screen.getByText(/email/i);
+    const passwordLabel = screen.getByText(/password/i);
+    expect(emailLabel.tagName).toBe('LABEL');
+    expect(passwordLabel.tagName).toBe('LABEL');
+  });
+
+  test('contains 1 button with text "OK"', () => {
+    const button = screen.getByRole('button', { name: /ok/i });
+    expect(button).toBeInTheDocument();
+  });
 });
