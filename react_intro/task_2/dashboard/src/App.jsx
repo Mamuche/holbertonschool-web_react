b984592dc620b523d6543de83a88ee7d562e04a5
Mamuche <marionlaroche17@gmail.com>
2025-07-29 10:15:01 +0200

    added ref even though it's useless because I use htmlFor

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,9 +1,13 @@
-import logo from './assets/holberton-logo.jpg';
 import './App.css'
-import { getCurrentYear, getFooterCopy } from './utils';
+import { useRef } from 'react';
 import Notifications from './Notifications';
+import logo from './assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from './utils';
+
 
 function App() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
   return (
     <>
@@ -21,11 +25,11 @@ function App() {
         <div className="App-body">
           <p>Login to access the full dashboard</p>
           <form>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <label htmlFor="email" onClick={() => emailRef.current && emailRef.current.focus()}>Email</label>
+            <input type="email" name="email" id="email" ref={emailRef} />
 
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <label htmlFor="password" onClick={() => passwordRef.current && passwordRef.current.focus()}>Password</label>
+            <input type="password" name="password" id="password" ref={passwordRef} />
 
             <button type="submit">OK</button>
           </form>
