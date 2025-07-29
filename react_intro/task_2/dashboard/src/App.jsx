b986bc96e8b8e1d7e95e972c36bcb5e45c40dc49
Mamuche <marionlaroche17@gmail.com>
2025-07-29 11:41:08 +0200

    task5

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -21,11 +21,11 @@ function App() {
 
         <div className="App-body">
           <p>Login to access the full dashboard</p>
-          <form>
-            <label htmlFor="email">Email</label>
+          <form className="App-form">
+            <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" />
 
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" />
 
             <button type="submit">OK</button>
