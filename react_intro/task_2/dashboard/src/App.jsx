30b044ada9e431db492bb1a7a4cd8da2571f80f4
Mamuche <marionlaroche17@gmail.com>
2025-07-29 10:16:44 +0200

    remove ref because it's useless

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,13 +1,10 @@
-import './App.css'
-import { useRef } from 'react';
-import Notifications from './Notifications';
 import logo from './assets/holberton-logo.jpg';
+import './App.css'
 import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
 
 
 function App() {
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
 
   return (
     <>
@@ -25,11 +22,11 @@ function App() {
         <div className="App-body">
           <p>Login to access the full dashboard</p>
           <form>
-            <label htmlFor="email" onClick={() => emailRef.current && emailRef.current.focus()}>Email</label>
-            <input type="email" name="email" id="email" ref={emailRef} />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
 
-            <label htmlFor="password" onClick={() => passwordRef.current && passwordRef.current.focus()}>Password</label>
-            <input type="password" name="password" id="password" ref={passwordRef} />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
 
             <button type="submit">OK</button>
           </form>
