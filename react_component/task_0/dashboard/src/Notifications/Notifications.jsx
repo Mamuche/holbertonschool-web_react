8e0e5dc347b4559bee4d7d0830a2b64816fb0572
Mamuche <marionlaroche17@gmail.com>
2025-08-04 15:44:17 +0200

    init project + task0

---
+++ react_component/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,53 @@
+import './Notifications.css';
+import React from 'react';
+import closeIcon from '../assets/close-button.png';
+import NotificationItem from './NotificationItem';
+
+function Notifications({ displayDrawer = false, list = [] }) {
+  return (
+    <>
+      <div className="notifications-title">
+        Your notifications
+      </div>
+
+      {displayDrawer && (
+        <div className="notification">
+          {list.length === 0 ? (
+            <p className="empty-notification">No new notification for now</p>
+          ) : (
+            <>
+              <button
+                style={{
+                  position: 'absolute',
+                  top: '10px',
+                  right: '10px',
+                  background: 'transparent',
+                  border: 'none',
+                  cursor: 'pointer'
+                }}
+                aria-label="Close"
+                onClick={() => console.log('Close button has been clicked')}
+              >
+                <img src={closeIcon} alt="close icon" />
+              </button>
+
+              <p>Here is the list of notifications</p>
+              <ul>
+                {list.map((notif) => (
+                  <NotificationItem
+                    key={notif.id}
+                    type={notif.type}
+                    html={notif.html}
+                    value={notif.value}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Notifications;
