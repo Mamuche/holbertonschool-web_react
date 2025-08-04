2860df80380fa32126e46a11c513de1732de75e8
Mamuche <marionlaroche17@gmail.com>
2025-08-04 11:25:29 +0200

    Remove all projects

--- react_props/task_0/dashboard/src/Footer/Footer.spec.js
+++
@@ -1,9 +0,0 @@
-import Footer from './Footer';
-import { render, screen } from '@testing-library/react';
-
-
-test('footer paragraphs', () => {
-	render(<Footer/>)
-    const footerText = screen.getByText(/copyright.*holberton school/i);
-    expect(footerText).toBeInTheDocument();
-});
